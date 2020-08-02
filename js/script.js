a81ecbaa27d39b071278f0215c37339f3f0c8d68
Denis <tardisThome@gmail.com>
2020-08-02 14:25:24 +0300

    learned timer

--- js/script.js
+++ js/script.js
@@ -42,5 +42,63 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    //Timer
+
+    const deadline = '2020-08-11';
+
+    function getTimeReamining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) %  60);
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) { 
+            return `0${num}`; 
+
+        } else {
+            return num;
+        }
+    }
+
+    function setCLock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            updateClock();
+
+            function updateClock() {
+                const t = getTimeReamining(endtime);
+
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                }
+            }
+
+
+    }
+
+    setCLock('.timer', deadline);
+
+
+
 
 });
